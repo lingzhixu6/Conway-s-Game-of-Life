6fea491f65fef811e2d9a25e473afbd2a272ba7e
Lingzhi <lingzhixujohn@gmail.com>
2020-01-08 16:25:05 +0000

    Version 2 done

--- src/ConwaysGameOfLife_VERSION2.java
+++ src/ConwaysGameOfLife_VERSION2.java
@@ -9,7 +9,7 @@ import javax.swing.*;
  * Conway's game of life is a cellular automaton devised by the
  * mathematician John Conway.
  */
-public class ConwaysGameOfLife_VERSION1 extends JFrame implements ActionListener {
+public class ConwaysGameOfLife_VERSION2 extends JFrame implements ActionListener {
     private static final Dimension DEFAULT_WINDOW_SIZE = new Dimension(800, 600);
     private static final Dimension MINIMUM_WINDOW_SIZE = new Dimension(400, 400);
     private static final int BLOCK_SIZE = 10;
@@ -26,18 +26,18 @@ public class ConwaysGameOfLife_VERSION1 extends JFrame implements ActionListener
 
     public static void main(String[] args) {
         // Setup the swing specifics
-        JFrame game = new ConwaysGameOfLife_VERSION1();
+        JFrame game = new ConwaysGameOfLife_VERSION2();
         game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         game.setTitle("Conway's Game of Life");
-        game.setIconImage(new ImageIcon(ConwaysGameOfLife_VERSION1.class.getResource("/images/logo.png")).getImage());
+        game.setIconImage(new ImageIcon(ConwaysGameOfLife_VERSION2.class.getResource("/images/logo.png")).getImage());
         game.setSize(DEFAULT_WINDOW_SIZE);
         game.setMinimumSize(MINIMUM_WINDOW_SIZE);
-        game.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - game.getWidth())/2,
-                (Toolkit.getDefaultToolkit().getScreenSize().height - game.getHeight())/2);
+        game.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - game.getWidth()) / 2,
+                (Toolkit.getDefaultToolkit().getScreenSize().height - game.getHeight()) / 2);
         game.setVisible(true);
     }
 
-    public ConwaysGameOfLife_VERSION1() {
+    public ConwaysGameOfLife_VERSION2() {
         // Setup menu
         mb_menu = new JMenuBar();
         setJMenuBar(mb_menu);
@@ -89,7 +89,7 @@ public class ConwaysGameOfLife_VERSION1 extends JFrame implements ActionListener
         } else {
             mi_game_play.setEnabled(true);
             mi_game_stop.setEnabled(false);
-            game.interrupt();
+            game.interrupt();   //interrupt() cannot stop the while loop
         }
     }
 
@@ -102,22 +102,22 @@ public class ConwaysGameOfLife_VERSION1 extends JFrame implements ActionListener
             // Put up an options panel to change the number of moves per second
             final JFrame f_options = new JFrame();
             f_options.setTitle("Options");
-            f_options.setSize(300,60);
-            f_options.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - f_options.getWidth())/2,
-                    (Toolkit.getDefaultToolkit().getScreenSize().height - f_options.getHeight())/2);
+            f_options.setSize(300, 60);
+            f_options.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - f_options.getWidth()) / 2,
+                    (Toolkit.getDefaultToolkit().getScreenSize().height - f_options.getHeight()) / 2);
             f_options.setResizable(false);
             JPanel p_options = new JPanel();
             p_options.setOpaque(false);
             f_options.add(p_options);
             p_options.add(new JLabel("Number of moves per second:"));
-            Integer[] secondOptions = {1,2,3,4,5,10,15,20};
+            Integer[] secondOptions = {1, 2, 3, 4, 5, 10, 15, 20};
             final JComboBox cb_seconds = new JComboBox(secondOptions);
             p_options.add(cb_seconds);
             cb_seconds.setSelectedItem(i_movesPerSecond);
-            cb_seconds.addActionListener(new ActionListener(){
+            cb_seconds.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent ae) {
-                    i_movesPerSecond = (Integer)cb_seconds.getSelectedItem();
+                    i_movesPerSecond = (Integer) cb_seconds.getSelectedItem();
                     f_options.dispose();
                 }
             });
@@ -126,14 +126,14 @@ public class ConwaysGameOfLife_VERSION1 extends JFrame implements ActionListener
             final JFrame f_autoFill = new JFrame();
             f_autoFill.setTitle("Autofill");
             f_autoFill.setSize(360, 60);
-            f_autoFill.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - f_autoFill.getWidth())/2,
-                    (Toolkit.getDefaultToolkit().getScreenSize().height - f_autoFill.getHeight())/2);
+            f_autoFill.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - f_autoFill.getWidth()) / 2,
+                    (Toolkit.getDefaultToolkit().getScreenSize().height - f_autoFill.getHeight()) / 2);
             f_autoFill.setResizable(false);
             JPanel p_autoFill = new JPanel();
             p_autoFill.setOpaque(false);
             f_autoFill.add(p_autoFill);
             p_autoFill.add(new JLabel("What percentage should be filled? "));
-            Object[] percentageOptions = {"Select",5,10,15,20,25,30,40,50,60,70,80,90,95};
+            Object[] percentageOptions = {"Select", 5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 95};
             final JComboBox cb_percent = new JComboBox(percentageOptions);
             p_autoFill.add(cb_percent);
             cb_percent.addActionListener(new ActionListener() {
@@ -141,7 +141,7 @@ public class ConwaysGameOfLife_VERSION1 extends JFrame implements ActionListener
                 public void actionPerformed(ActionEvent e) {
                     if (cb_percent.getSelectedIndex() > 0) {
                         gb_gameBoard.resetBoard();
-                        gb_gameBoard.randomlyFillBoard((Integer)cb_percent.getSelectedItem());
+                        gb_gameBoard.randomlyFillBoard((Integer) cb_percent.getSelectedItem());
                         f_autoFill.dispose();
                     }
                 }
@@ -167,9 +167,11 @@ public class ConwaysGameOfLife_VERSION1 extends JFrame implements ActionListener
     }
 
     private class GameBoard extends JPanel {
-        private Dimension d_gameBoardSize = null;
+        private volatile Dimension d_gameBoardSize = null;                       //Only modified by one thread and read by multiple threads. Thus 'volatile' can be applied to it.
         private ArrayList<Point> point = new ArrayList<Point>(0);
         private GameBoardListener gameBoardListener = new GameBoardListener();
+        private final Object paintLock = new Object();
+        private final Object resizeLock = new Object();
 
         public GameBoard() {
             // Add resizing listener
@@ -180,43 +182,55 @@ public class ConwaysGameOfLife_VERSION1 extends JFrame implements ActionListener
 
         private void updateArraySize() {
             ArrayList<Point> removeList = new ArrayList<Point>(0);
-            for (Point current : point) {
-                if ((current.x > d_gameBoardSize.width-1) || (current.y > d_gameBoardSize.height-1)) {
-                    removeList.add(current);
+            synchronized (paintLock) {
+                for (Point current : point) {
+                    if ((current.x > d_gameBoardSize.width - 1) || (current.y > d_gameBoardSize.height - 1)) {
+                        removeList.add(current);
+                    }
                 }
+                point.removeAll(removeList);
+                repaint();
             }
-            point.removeAll(removeList);
-            repaint();
         }
 
         public void addPoint(int x, int y) {
-            if (!point.contains(new Point(x,y))) {
-                point.add(new Point(x,y));
+            synchronized (paintLock) {
+                if (!point.contains(new Point(x, y))) {
+                    point.add(new Point(x, y));
+                }
+                repaint();
             }
-            repaint();
         }
 
         public void addPoint(MouseEvent me) {
-            int x = me.getPoint().x/BLOCK_SIZE-1;
-            int y = me.getPoint().y/BLOCK_SIZE-1;
-            if ((x >= 0) && (x < d_gameBoardSize.width) && (y >= 0) && (y < d_gameBoardSize.height)) {
-                addPoint(x,y);
+            int x = me.getPoint().x / BLOCK_SIZE - 1;
+            int y = me.getPoint().y / BLOCK_SIZE - 1;
+            synchronized (resizeLock) {
+                if ((x >= 0) && (x < d_gameBoardSize.width) && (y >= 0) && (y < d_gameBoardSize.height)) {
+                    addPoint(x, y);
+                }
             }
         }
 
         public void removePoint(int x, int y) {
-            point.remove(new Point(x,y));
+            synchronized (paintLock) {
+                point.remove(new Point(x, y));
+            }
         }
 
         public void resetBoard() {
-            point.clear();
+            synchronized (paintLock) {
+                point.clear();
+            }
         }
 
         public void randomlyFillBoard(int percent) {
-            for (int i=0; i<d_gameBoardSize.width; i++) {
-                for (int j=0; j<d_gameBoardSize.height; j++) {
-                    if (Math.random()*100 < percent) {
-                        addPoint(i,j);
+            synchronized (resizeLock) {
+                for (int i = 0; i < d_gameBoardSize.width; i++) {
+                    for (int j = 0; j < d_gameBoardSize.height; j++) {
+                        if (Math.random() * 100 < percent) {
+                            addPoint(i, j);
+                        }
                     }
                 }
             }
@@ -226,103 +240,156 @@ public class ConwaysGameOfLife_VERSION1 extends JFrame implements ActionListener
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
             try {
-                for (Point newPoint : point) {
-                    // Draw new point
-                    g.setColor(Color.blue);
-                    g.fillRect(BLOCK_SIZE + (BLOCK_SIZE*newPoint.x), BLOCK_SIZE + (BLOCK_SIZE*newPoint.y), BLOCK_SIZE, BLOCK_SIZE);
+                synchronized (paintLock) {
+                    for (Point newPoint : point) {
+                        // Draw new point
+                        g.setColor(Color.blue);
+                        g.fillRect(BLOCK_SIZE + (BLOCK_SIZE * newPoint.x), BLOCK_SIZE + (BLOCK_SIZE * newPoint.y), BLOCK_SIZE, BLOCK_SIZE);
+                    }
                 }
             } catch (ConcurrentModificationException cme) {
-                System.out.println("!!!");
+                System.out.println("CONCURRENCY EXCEPTION !!!");
             }
             // Setup grid
             g.setColor(Color.BLACK);
-            for (int i=0; i<=d_gameBoardSize.width; i++) {
-                g.drawLine(((i*BLOCK_SIZE)+BLOCK_SIZE), BLOCK_SIZE, (i*BLOCK_SIZE)+BLOCK_SIZE, BLOCK_SIZE + (BLOCK_SIZE*d_gameBoardSize.height));
-            }
-            for (int i=0; i<=d_gameBoardSize.height; i++) {
-                g.drawLine(BLOCK_SIZE, ((i*BLOCK_SIZE)+BLOCK_SIZE), BLOCK_SIZE*(d_gameBoardSize.width+1), ((i*BLOCK_SIZE)+BLOCK_SIZE));
+            synchronized (resizeLock) {
+                for (int i = 0; i <= d_gameBoardSize.width; i++) {
+                    g.drawLine(((i * BLOCK_SIZE) + BLOCK_SIZE), BLOCK_SIZE, (i * BLOCK_SIZE) + BLOCK_SIZE, BLOCK_SIZE + (BLOCK_SIZE * d_gameBoardSize.height));
+                }
+                for (int i = 0; i <= d_gameBoardSize.height; i++) {
+                    g.drawLine(BLOCK_SIZE, ((i * BLOCK_SIZE) + BLOCK_SIZE), BLOCK_SIZE * (d_gameBoardSize.width + 1), ((i * BLOCK_SIZE) + BLOCK_SIZE));
+                }
             }
         }
-
 
         private class GameBoardListener implements ComponentListener, MouseListener, MouseMotionListener {
             @Override
             public void componentResized(ComponentEvent e) {
                 // Setup the game board size with proper boundries
-                d_gameBoardSize = new Dimension(getWidth()/BLOCK_SIZE-2, getHeight()/BLOCK_SIZE-2);
-                updateArraySize();
+                synchronized (resizeLock) {
+                    d_gameBoardSize = new Dimension(getWidth() / BLOCK_SIZE - 2, getHeight() / BLOCK_SIZE - 2);
+                    updateArraySize();
+                }
             }
+
             @Override
-            public void componentMoved(ComponentEvent e) {}
+            public void componentMoved(ComponentEvent e) {
+            }
+
             @Override
-            public void componentShown(ComponentEvent e) {}
+            public void componentShown(ComponentEvent e) {
+            }
+
             @Override
-            public void componentHidden(ComponentEvent e) {}
+            public void componentHidden(ComponentEvent e) {
+            }
+
             @Override
-            public void mouseClicked(MouseEvent e) {}
+            public void mouseClicked(MouseEvent e) {
+            }
+
             @Override
-            public void mousePressed(MouseEvent e) {}
+            public void mousePressed(MouseEvent e) {
+            }
+
             @Override
             public void mouseReleased(MouseEvent e) {
                 // Mouse was released (user clicked)
                 addPoint(e);
             }
-            @Override
-            public void mouseEntered(MouseEvent e) {}
 
             @Override
-            public void mouseExited(MouseEvent e) {}
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
 
             @Override
             public void mouseDragged(MouseEvent e) {
                 // Mouse is being dragged, user wants multiple selections
                 addPoint(e);
             }
+
             @Override
-            public void mouseMoved(MouseEvent e) {}
+            public void mouseMoved(MouseEvent e) {
+            }
         }
 
         private class GameThread implements Runnable {
             @Override
             public void run() {
-                while(true) {
-                    boolean[][] gameBoard = new boolean[d_gameBoardSize.width+2][d_gameBoardSize.height+2];
-                    for (Point current : point) {
-                        gameBoard[current.x+1][current.y+1] = true;
+                while (true) {
+//                   System.err.println(java.lang.Thread.activeCount());
+//                    Set<Thread> threads = Thread.getAllStackTraces().keySet();
+//                    for (Thread t : threads) {
+//                        String name = t.getName();
+//                        Thread.State state = t.getState();
+//                        int priority = t.getPriority();
+//                        String type = t.isDaemon() ? "Daemon" : "Normal";
+//                        System.out.printf("%-20s \t %s \t %d \t %s\n", name, state, priority, type);
+//                    }
+//                   System.out.println(Thread.currentThread().toString());
+                    boolean[][] gameBoard;
+                    synchronized (paintLock) {
+                        gameBoard = new boolean[d_gameBoardSize.width + 2][d_gameBoardSize.height + 2];
+
+                        for (Point current : point) {
+                            gameBoard[current.x + 1][current.y + 1] = true;
+                        }
                     }
                     ArrayList<Point> survivingCells = new ArrayList<Point>(0);
                     // Iterate through the array, follow game of life rules
-                    for (int i=1; i<gameBoard.length-1; i++) {
-                        for (int j=1; j<gameBoard[0].length-1; j++) {
+                    for (int i = 1; i < gameBoard.length - 1; i++) {
+                        for (int j = 1; j < gameBoard[0].length - 1; j++) {
                             int surrounding = 0;
-                            if (gameBoard[i-1][j-1]) { surrounding++; }
-                            if (gameBoard[i-1][j])   { surrounding++; }
-                            if (gameBoard[i-1][j+1]) { surrounding++; }
-                            if (gameBoard[i][j-1])   { surrounding++; }
-                            if (gameBoard[i][j+1])   { surrounding++; }
-                            if (gameBoard[i+1][j-1]) { surrounding++; }
-                            if (gameBoard[i+1][j])   { surrounding++; }
-                            if (gameBoard[i+1][j+1]) { surrounding++; }
+                            if (gameBoard[i - 1][j - 1]) {
+                                surrounding++;
+                            }
+                            if (gameBoard[i - 1][j]) {
+                                surrounding++;
+                            }
+                            if (gameBoard[i - 1][j + 1]) {
+                                surrounding++;
+                            }
+                            if (gameBoard[i][j - 1]) {
+                                surrounding++;
+                            }
+                            if (gameBoard[i][j + 1]) {
+                                surrounding++;
+                            }
+                            if (gameBoard[i + 1][j - 1]) {
+                                surrounding++;
+                            }
+                            if (gameBoard[i + 1][j]) {
+                                surrounding++;
+                            }
+                            if (gameBoard[i + 1][j + 1]) {
+                                surrounding++;
+                            }
                             if (gameBoard[i][j]) {
                                 // Cell is alive, Can the cell live? (2-3)
                                 if ((surrounding == 2) || (surrounding == 3)) {
-                                    survivingCells.add(new Point(i-1,j-1));
+                                    survivingCells.add(new Point(i - 1, j - 1));
                                 }
                             } else {
                                 // Cell is dead, will the cell be given birth? (3)
                                 if (surrounding == 3) {
-                                    survivingCells.add(new Point(i-1,j-1));
+                                    survivingCells.add(new Point(i - 1, j - 1));
                                 }
                             }
                         }
                     }
-                    resetBoard();
-                    point.addAll(survivingCells);
-                    repaint();
+                    synchronized (paintLock) {
+                        resetBoard();
+                        point.addAll(survivingCells);
+                        repaint();
+                    }
                     try {
-                        Thread.sleep(1000/i_movesPerSecond);
-                        //replace recursive run() with the big while loop
-                    } catch (InterruptedException ex) {
+                        Thread.sleep(1000 / i_movesPerSecond);
+                        //replace recursive run() here with the big while loop
+                    } catch (InterruptedException ex) {              //InterruptedException means that another thread is trying to shut down this thread.
                         break;
                     }
                 }
